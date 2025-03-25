a1f51b026e37d289736c4a70f289f42c6cbfc7aa
chay <cami990129@gs.utm.mx>
2025-03-25 11:10:47 -0600

    proyecto profesor

---
+++ proyecto-rutas/app/tools/navbar/NavBar.tsx
@@ -0,0 +1,9 @@
+export const NavBar = () => {
+    return (
+        <nav className="flex  bg-amber-700 bg-opacity-30 p-2 m-3 rounded">
+            <a className="m-2 text-8xl" href="/">Home</a>
+            <a className="m-2" href="/product">Product</a>
+            <a className="m-2" href="/about">about</a>
+        </nav>
+    )
+}
